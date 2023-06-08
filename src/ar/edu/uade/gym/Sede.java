fbb75b6c89814b63d9c12f328835b04f7cd57547
reborafs <reborafs@gmail.com>
2023-06-07 21:15:59 -0300

    Implementacion de metodo agregarSede con test

--- src/ar/edu/uade/gym/Sede.java
+++ src/ar/edu/uade/gym/Sede.java
@@ -6,15 +6,16 @@ import ar.edu.uade.articulos.Articulo;
 public class Sede {
     private String ubicacion;
     private TipoNivel tipoNivel;
-    private int capacidad;
     private ArrayList<Articulo> stockArticulos;
+    private ArrayList<Emplazamiento> emplazamientosDisponibles;
     private ArrayList<Clase> ArrayListaClases;
     private ArrayList<Ejercicio> ejerciciosDisponibles;
 
-    public Sede(String ubicacion, TipoNivel tipoNivel, int capacidad) {
-    	this.ubicacion = ubicacion;
+    public Sede(String ubicacion, TipoNivel tipoNivel, ArrayList<Emplazamiento> emplazamientos, ArrayList<Ejercicio> ejercicios) {
+    	this.ubicacion = ubicacion.toLowerCase();
     	this.tipoNivel = tipoNivel;
-    	this.capacidad = capacidad;
+    	this.emplazamientosDisponibles = emplazamientos;
+    	this.ejerciciosDisponibles = ejercicios;
     }
     
     public int calcularRentabilidad() {
@@ -25,5 +26,18 @@ public class Sede {
     public int getDesgasteArticulo() {
         // Code for getting article wear and tear
         return 0;
+    }
+    
+    public String getUbicacion() {
+        return this.ubicacion;
+    }
+    
+    public TipoNivel getTipoNivel() {
+        return this.tipoNivel;
+    }
+    
+    @Override
+    public String toString() {
+    	return "["+this.ubicacion+","+this.tipoNivel.toString()+"]";
     }
 }
--- src/ar/edu/uade/main/Main.java
+++ src/ar/edu/uade/main/Main.java
@@ -1,10 +1,30 @@
 package ar.edu.uade.main;
 
+import ar.edu.uade.gym.CadenaGimnasio;
+import ar.edu.uade.gym.GymException;
+import ar.edu.uade.gym.TipoNivel;
+import ar.edu.uade.usuarios.Usuario;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// INICIO CADENA GIMNASIO
+		CadenaGimnasio gym = CadenaGimnasio.getInstance();
+		
+		try {
+			// ESTAS SEDE SE AGREGAN BIEN
+			gym.agregarSede("Caballito", TipoNivel.BLACK, null, null);
+			gym.agregarSede("Belgrano", TipoNivel.ORO, null, null);		
+			
+			// ESTAS SEDES VAN A FALLAR PORQUE CABALLITO YA EXISTE
+			gym.agregarSede("Caballito", TipoNivel.ORO, null, null);
+			gym.agregarSede("Palermo", TipoNivel.PLATINUM, null, null);
+		} catch (GymException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(gym.getStringListaSedes());
 	}
 
 }
